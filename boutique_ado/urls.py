95ee9f0814e292d31604568720c014a8579adc64
Krisztina-sketch <krisztinatoth19821227@gmail.com>
2026-09-06 21:30:00 +0100

    Fix uploaded media display on deployed site

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 
 urlpatterns = [
@@ -10,4 +10,10 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('accounts/', include('allauth.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+]
